80a3a20a12f1de9559cbc507163c2a1d9d85397b
Rijurajx <iphonerijuraj@gmail.com>
2024-09-28 18:28:33 +0530

    added landing page

---
+++ app/components/landing/calltoaction.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Button from "../global/button_gradient";
+import "./landing.css"
+const CallToAction = () => {
+  return (
+    <div className="relative min-h-screen">
+      {/* Background image */}
+      <img
+        src="/landing-page-assets/landing_cta.png"
+        className="absolute inset-0 w-full h-full object-cover"
+        alt="Background"
+      />
+
+      {/* Dark Overlay for Better Text Visibility */}
+      {/* <div className="absolute inset-0 bg-black opacity-30"></div> */}
+
+      {/* Overlay content */}
+      <div className="relative z-10 flex flex-col justify-center h-full pl-12 text-left text-white pt-40">
+        {/* Main Text */}
+        <h1
+          className="text-8xl  mb-4 viga-regular"
+          style={{ textShadow: "4px 4px 10px rgba(0, 0, 0, 0.7)" }}
+        >
+          Venture Beyond the <br /> Stars
+        </h1>
+        <p
+          className="text-2xl font-semibold mb-6 opacity-85"
+          style={{ textShadow: "3px 3px 10px rgba(0, 0, 0, 0.6)" }}
+        >
+          Experience the Cosmos Like Never Before: In an Immersive Simulation
+        </p>
+
+        {/* Sub-text */}
+        <p
+          className="text-sm max-w-md mb-8 font-light opacity-80"
+          style={{ textShadow: "2px 2px 8px rgba(0, 0, 0, 0.5)" }}
+        >
+          Embark on Antarikxplorer’s VR and Gyro immersive experience and
+          explore distant exoplanets from the comfort of your space. Soar
+          through alien worlds, navigate star systems, and interact with
+          real-time NASA data. Your cosmic adventure awaits—are you ready to
+          explore?
+        </p>
+
+        {/* Button */}
+        <Button className="w-80">Launch your Virtual Journey</Button>
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
+// Local fonts setup
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,6 +22,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+          
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet"></link>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
